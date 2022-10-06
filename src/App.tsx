33352ff742edfb04a53e1fc87e42b7d941de3096
felipe.domingues <felipe.domingues@calix.com>
2022-10-06 19:26:14 -0300

    update app and index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
 import React from "react";
 import "./App.css";
-import { graphql } from "babel-plugin-relay/macro";
-import {
-  RelayEnvironmentProvider,
-  loadQuery,
-  usePreloadedQuery,
-} from "react-relay/hooks";
-import RelayEnvironment from "./RelayEnvironment";
-
+import { Home } from "./pages";
 const { Suspense } = React;
 
-// Define a query
-const AuthorsNameQuery = graphql`
-  query AppAuthorsNameQuery {
-    authors {
-      firstName
-    }
-  }
-`;
-
-const preloadedQuery = loadQuery(RelayEnvironment, AuthorsNameQuery, {
-  /* query variables */
-});
-
-function App(props: any) {
-  const data: any = usePreloadedQuery(AuthorsNameQuery, props.preloadedQuery);
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          {data.authors.map((author: { firstName: string }) => {
-            return <p>{author.firstName}</p>;
-          })}
-        </div>
+        <Home />
       </header>
     </div>
   );
@@ -41,11 +15,9 @@ function App(props: any) {
 
 function AppRoot(props: any) {
   return (
-    <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <Suspense fallback={"Loading..."}>
-        <App preloadedQuery={preloadedQuery} />
-      </Suspense>
-    </RelayEnvironmentProvider>
+    <Suspense fallback={"Loading..."}>
+      <App />
+    </Suspense>
   );
 }
 
